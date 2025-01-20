31f5d3f3fc8efd9b3d8e6331a78d940490f94eda
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2025-01-20 21:44:16 +0000

    adjusted css for song title padding

--- src/components/SongTitle.jsx
+++ src/components/SongTitle.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SongTitle = () => {
   return (
     <div className="w-400"> 
-      <h2 className="font-inter text-2xl text-black font-bold h-29">Song Title</h2>
+      <h2 className="font-inter text-2xl text-black font-bold h-29 pt-3">Song Title</h2>
       <p className="font-inter text-sm text-gray-400 h-16">Author</p>
     </div>
   );
