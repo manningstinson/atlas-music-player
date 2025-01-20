52041e22cb00415aa89276b62dca2366e7dcd5b9
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2025-01-20 18:08:25 +0000

    added responsive tailwind css to music player

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,7 +1,9 @@
-export default function MusicPlayer() {
-  return <div class="bg-white w-[896px] h-[640px] border border-black opacity-100">
-    
-</div>
-  
 
+export default function MusicPlayer() {
+  return <div className="w-player-large h-player-large md:w-player-small md:h-player-small">
+  {
+  /* Your music player content here */
+  
+  }
+</div>
 }
