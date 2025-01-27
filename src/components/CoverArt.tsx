cf9767b5afd9d286f0fe860a0cf0492b719b99b3
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2025-01-27 08:15:38 +0000

    added typescript to cover art to dynamically load

--- src/components/CoverArt.tsx
+++ src/components/CoverArt.tsx
@@ -1,11 +1,29 @@
-import coverArt from '../assets/painted-in-blue.jpeg';
+import defaultCoverArt from '../assets/painted-in-blue.jpeg';
 
-const CoverArt = () => {
-return (
-  <div className="w-full h-full">
-    <img src={coverArt} alt="Album Cover" className="w-full h-full rounded-lg" />
-  </div>
-);
+interface Song {
+  id: string;
+  title: string;
+  artist: string;
+  albumArt: string;
+}
+
+interface CoverArtProps {
+  currentSong: Song | null;
+}
+
+const CoverArt: React.FC<CoverArtProps> = ({ currentSong }) => {
+  // Added console.log to check what image is being used
+  console.log('Current album art:', currentSong?.albumArt || 'Using default image');
+
+  return (
+    <div className="w-full h-full">
+      <img 
+        src={currentSong?.albumArt || defaultCoverArt} 
+        alt="Album Cover" 
+        className="w-full h-full rounded-lg" 
+      />
+    </div>
+  );
 }
 
 export default CoverArt;
